bb4a7e4a501e1d021fc120f9e9d17f5a8021962d
Esmaam <148785688+Esmaam@users.noreply.github.com>
2024-11-07 14:34:49 +0100

    Add [Stack] [FinalStack] cards stacks

---
+++ LogicLayer/FinalStack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer
+{
+    internal class FinalStack : Stack
+    {
+        public bool IsComplete
+        {
+            get { return Cards.Count == 13; }
+        }
+    }
+}
---
+++ LogicLayer/Stack.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer
+{
+    public class Stack
+    {
+        private List<Card> cards;
+        public List<Card> Cards { get => cards; }
+
+        public Card GetLastCard
+        {
+            get { return this.cards.Last(); }
+        }
+    }
+}
